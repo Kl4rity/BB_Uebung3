755b6dde3bc92031541398bdaf42ece46a243728
Kl4rity <clemens@stift.online>
2018-05-08 11:02:54 +0200

    implemented getList function routing functions to correct SQL query. Capsulated all specific functions.

--- models/ListModel.php
+++ models/ListModel.php
@@ -10,8 +10,49 @@ class ListModel {
         $this->database = new Database(DBHost, DBName, DBUser, DBPass);
     }
     
-    public function listProjects(){
+    public function getList($requestParams){
+        $parentID = $requestParams["parentID"];
+        
+        switch($requestParams["listType"]):
+            case "projects":
+                return listProjects();
+            case "floors":
+                return listFloors($parentID);
+            case "rooms":
+                return listRooms($parentID);
+            case "devices":
+                return listDevices($parentID);
+            case "sensors":
+                return listSensors($parentID);
+        endswitch;
+    }
+    
+    // TODO create public function generateQuery(){}
+    // Should take a keyword and, if necessary, a parentId
+    // argument should be a dictionary with 2 entries  -listtype and parentID
+    
+    private function listProjects(){
         $sql = " SELECT id, name FROM projects ";
+        return $this->database->query($sql);
+    }
+    
+    private function listFloors($parentID){
+        $sql = " SELECT id, name, floor_count_from_basement, created FROM floors WHERE projects_id = " . $parentID . " ";
+        return $this->database->query($sql);
+    }
+    
+    private function listRooms($parentID){
+        $sql = " SELECT id, name, created FROM rooms WHERE floors_id = " . $parentID . " ";
+        return $this->database->query($sql);
+    }
+    
+    private function listDevices($parentID){
+        $sql = " SELECT id, name, created FROM devices WHERE rooms_id = " . $parentID . " ";
+        return $this->database->query($sql);
+    }
+    
+    private function listSensors($parentID){
+        $sql = " SELECT id, name, unit, value, created FROM sensors WHERE devices_id = " . $parentID . " ";
         return $this->database->query($sql);
     }
 }
