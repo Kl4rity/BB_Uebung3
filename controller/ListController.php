d148491afadaf1638bd2771c31d2d80cc7edfd0e
Kl4rity <clemens@stift.online>
2018-05-08 12:36:56 +0200

    Generalized formatAndDisplayData logic. Works with any level of the data-hierarchy now.

--- controller/ListController.php
+++ controller/ListController.php
@@ -13,32 +13,48 @@ class ListController {
     public function route(){
         $listModel = new ListModel();
         
-        $data;
+        $listData;
         
-        print_r(array_keys($_GET));
+        // Checks whether a listtype was given - provides a default (PROJECTS) if none was given.
+        if (!array_key_exists("listType", $_GET)){
+            $requestParams["listType"] = "PROJECTS";
+        } else {
+            $requestParams["listType"] = $_GET["listType"];   
+        }
         
-        $data = $listModel->listProjects();
+        // Checks whether parentID exists and provides a default if it doesn't
+        if (!array_key_exists("parentID", $_GET)){
+            $requestParams["parentID"] = "NONE";
+        } else {
+            $requestParams["parentID"] = $_GET["parentID"];
+        }
+                
+        $listData = $listModel->getList($requestParams);
         
-        // TODO Differentiate between different URLs
-        
-        // TODO Route depending on params given
-        
-        $this->formatAndDisplayData($data);
+        $this->formatAndDisplayData($listData);
     }
     
-    private function formatAndDisplayData($data){
+    private function formatAndDisplayData($listData){
+
+        $list = array();
         
-        $projectsList = array();
-        
-        foreach($data as $dbEntry){
-            $projectsList[] = array(
-                "name"=> $dbEntry['name'], 
-                "url"=> "http://".$_SERVER["SERVER_NAME"]."/Uebung3/index.php?listtype=FLOORS&parentid=".$dbEntry['id']
-            );
+        foreach($listData["queryData"] as $dbEntry){
+            // Generate holder for all the data fetched.
+            $listEntry = array();
+            // Get names for attribtues in table
+            foreach(array_keys(array_unique($dbEntry)) as $rowName){
+                // Fetch all data returned from SQL Query and fill it in
+                $listEntry[$rowName] = $dbEntry[$rowName];
+            }
+            // Generate and attach URL for further navigation (if not at last level already)
+            if($listData["nextLevel"] != "NONE"){
+                $listEntry["url"] = "http://".$_SERVER["SERVER_NAME"]."/Uebung3/index.php?listType=".$listData["nextLevel"]."&parentID=".$dbEntry['id'];
+            }
+            $list[] = $listEntry;
         }
         $outputData = array (
-            "listtype" => "Projects",
-            "items" => $projectsList
+            "listtype" => $listData["currentLevel"],
+            "items" => $list
         );
         $this->jsonView->streamOutput($outputData);
     }
